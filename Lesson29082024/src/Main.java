36328223ff5b78115ebe325f112dad8eb2e3254e
ArtemSG <artemsgor@gmail.com>
2024-08-29 13:06:59 +0200

    Lesson29082024 && chat history

---
+++ Lesson29082024/src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+//        int num = 5;
+//        unary -
+//        int numTwo = -num;
+//        System.out.println("num = " + num);
+//        System.out.println("numTwo = " + numTwo);
+//        System.out.println("-num = " + (-num));
+
+//        increment
+//        num++;
+//        num = num + 1;
+//        num += 1;
+
+//        postfix
+//        num++;
+//        System.out.println(num++);
+//        int tmp = num;
+//        System.out.println(tmp);
+
+//        prefix
+//        System.out.println(++num);
+
+//        decrement
+//        num--;
+//        num = num - 1;
+//        num -= 1;
+//        num--;
+//        postfix
+//        System.out.println(num--);
+//        System.out.println(num);
+
+//        prefix
+//        System.out.println(--num);
+
+//        int i = 5; // 0101
+//        int j = 2; // 0010
+//        i + j
+//        0101 // 5
+//        +
+//        0010 // 2
+//        --------------
+//        0111 // 7
+
+
+//        System.out.println(i);
+//        int j = i++ + ++i;
+//        System.out.println(j);
+//        System.out.println(i);
+        int a = 5;
+        int b = 10;
+        int c = 1;
+        if(a >= b && c < a) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+    }
+}
